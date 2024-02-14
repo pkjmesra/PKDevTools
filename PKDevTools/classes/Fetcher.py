f59171b23b5a85de0990a9b2c6a1314c4847841f
PK <pkjmesra@gmail.com>
2024-02-14 17:43:19 +0100

    Enhanced general timeout

--- PKDevTools/classes/Fetcher.py
+++ PKDevTools/classes/Fetcher.py
@@ -59,7 +59,7 @@ class StockDataEmptyException(Exception):
 class stubConfigManager:
     def __init__(self):
         self.maxNetworkRetryCount = 3
-        self.generalTimeout = 2
+        self.generalTimeout = 4
 
     def restartRequestsCache(self):
         return None
