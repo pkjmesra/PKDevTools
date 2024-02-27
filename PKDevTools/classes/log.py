d5c24a2c042421320920f13f614c6d8f9fb12901
PK <pkjmesra@gmail.com>
2024-02-27 23:38:01 +0100

    time measure

--- PKDevTools/classes/log.py
+++ PKDevTools/classes/log.py
@@ -377,6 +377,18 @@ def log_to(logger_func):
 
     return decorator
 
+def measure_time(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        # print('%r (%r, %r) %2.2f sec' % \
+        #     (f.__name__, args, kw, te-ts))
+        print('%r %2.2f sec' % \
+            (f.__name__, te-ts))
+        return result
+    return timed
 
 tracelog = log_to(trace_log)
 
