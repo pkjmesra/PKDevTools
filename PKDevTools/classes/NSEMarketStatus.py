a8075ee724dccc30187cf0abd6e6568b7547d0d6
PK <pkjmesra@gmail.com>
2024-05-27 22:39:13 +0200

    bug fix

--- PKDevTools/classes/NSEMarketStatus.py
+++ PKDevTools/classes/NSEMarketStatus.py
@@ -82,10 +82,11 @@ class NSEMarketStatus(SingletonMixin, metaclass=SingletonType):
         fileName = "nse_next_bell.txt"
         next_bell, filePath, modifiedDateTime = Archiver.findFileInAppResultsDirectory(fileName)
         curr = datetime.datetime.now(pytz.timezone("Asia/Kolkata"))
-        lastBellDate = datetime.datetime.strptime(next_bell.split("T")[0],"%Y-%m-%d")
-        shouldFetch = next_bell is None or (next_bell is not None and (curr.date() > modifiedDateTime.date() or curr.date() > lastBellDate.date()))
+        dtPart = next_bell.replace("T"," ").split("+")[0]
+        lastBellDateTime = datetime.datetime.strptime(dtPart,"%Y-%m-%d %H:%M:%S").replace(tzinfo=curr.tzinfo)
+        shouldFetch = next_bell is None or (next_bell is not None and (curr.date() > modifiedDateTime.date() or curr > lastBellDateTime))
         if shouldFetch:
-            scraper = cloudscraper.create_scraper()          
+            scraper = cloudscraper.create_scraper()
             url = "https://www.tradinghours.com/markets/nse-india"
             res = scraper.get(url)
             try:
